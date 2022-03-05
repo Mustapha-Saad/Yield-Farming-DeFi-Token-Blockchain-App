b1edfdfff756335121ad3f114b0faa91d727b339
Mustapha Saad <mustaphasaad321@gmail.com>
2022-03-05 05:58:44 +0100

    Add files via upload

---
+++ precache-manifest.f2b88ff632892e8166041c5cb315c876.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a81c60353a20725d46c0",
+    "url": "/Yield-Farming-DeFi-Token-Blockchain-App/static/css/main.91afecf0.chunk.css"
+  },
+  {
+    "revision": "a81c60353a20725d46c0",
+    "url": "/Yield-Farming-DeFi-Token-Blockchain-App/static/js/main.a81c6035.chunk.js"
+  },
+  {
+    "revision": "9bdd8c86df1c1d41ca8c",
+    "url": "/Yield-Farming-DeFi-Token-Blockchain-App/static/css/1.b083795a.chunk.css"
+  },
+  {
+    "revision": "9bdd8c86df1c1d41ca8c",
+    "url": "/Yield-Farming-DeFi-Token-Blockchain-App/static/js/1.9bdd8c86.chunk.js"
+  },
+  {
+    "revision": "c10d3477454370d0592a",
+    "url": "/Yield-Farming-DeFi-Token-Blockchain-App/static/js/runtime~main.c10d3477.js"
+  },
+  {
+    "revision": "da70255120845f3066c5bdcc7367f259",
+    "url": "/Yield-Farming-DeFi-Token-Blockchain-App/static/media/tether.da702551.png"
+  },
+  {
+    "revision": "8d345c4efaa0a76ddd62379282017dfb",
+    "url": "/Yield-Farming-DeFi-Token-Blockchain-App/index.html"
+  }
+];
